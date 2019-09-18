e5ccd9275d30e01e54916f3c20add45e351a730c
lianshufeng <251708339@qq.com>
2019-09-18 11:38:40 +0800

    项目初次提交

---
+++ Sniffer-Core/src/main/java/com/jpznm/dht/sniffercore/SniffercoreApplication.java
@@ -0,0 +1,12 @@
+package com.jpznm.dht.sniffercore;
+
+import com.fast.dev.core.boot.ApplicationBootSuper;
+import org.springframework.boot.SpringApplication;
+
+public class SniffercoreApplication extends ApplicationBootSuper {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SniffercoreApplication.class, args);
+    }
+
+}
